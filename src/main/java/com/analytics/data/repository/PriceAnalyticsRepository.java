f266d423230b33dc04f7a447d632f78e990c44fc
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-15 15:22:04 -0300

    feat: create repository Price Analytics

---
+++ src/main/java/com/analytics/data/repository/PriceAnalyticsRepository.java
@@ -0,0 +1,13 @@
+package com.analytics.data.repository;
+
+import com.analytics.data.entity.PriceAnalyticsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PriceAnalyticsRepository extends JpaRepository<PriceAnalyticsEntity, Long> {
+
+    Optional<PriceAnalyticsEntity> findByPrice(String aLong);
+}
