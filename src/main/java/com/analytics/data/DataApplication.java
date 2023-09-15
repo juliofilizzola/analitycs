bd084548d6cfae50f3b15928f22a08040bada3e2
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-15 15:09:27 -0300

    feat: modify name project and move files

--- src/main/java/com/analytics/data/DataApplication.java
+++ src/main/java/com/analytics/data/DataApplication.java
@@ -1,4 +1,4 @@
-package com.analitycs.data;
+package com.analytics.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/analytics/data/DataApplicationTests.java
+++ src/test/java/com/analytics/data/DataApplicationTests.java
@@ -1,4 +1,4 @@
-package com.analitycs.data;
+package com.analytics.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
